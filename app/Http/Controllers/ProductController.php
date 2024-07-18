a79c34b2a80a5305de642d588acd52f0162ab974
bmiit145 <21bmiit145@gmail.com>
2024-07-18 16:18:54 +0530

    product table chaneg

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -170,12 +170,11 @@ class ProductController extends Controller
         'sku' => 'nullable|string|max:255',
         'quantity' => 'nullable|integer',
         'IGI_certificate' => 'nullable|string|max:255',
-        'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        'photo' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         'gallery' => 'nullable|array',
         'gallery.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         'attributes' => 'nullable|array']
          );
-
 
         if ($request->hasFile('photo')) {
             $mainPhotoPath = $request->file('photo')->store('photos', 'public');
